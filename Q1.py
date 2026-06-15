0d498615e3bc2647fc8809a8cc31eeb088633fe0
Shivam Thakur <thakurshivam0609@gmail.com>
2026-06-16 00:06:56 +0530

    Add files via upload

---
+++ Q1.py
@@ -0,0 +1,5 @@
+name = "Shivam"
+
+print("Value is : ", name)
+print("Type is : ", type(name))
+print("Memory address is : ", id(name))
---
+++ Q2.py
@@ -0,0 +1,9 @@
+a = 10
+b = 10
+print(id(a))
+print(id(b))
+
+x = [10]
+y = [10]
+print(id(x))
+print(id(y))
